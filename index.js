f1b9f852a0d547101bf06ccbc523f8f52ec7f384
leandro-cloud <134974518+leandro-cloud@users.noreply.github.com>
2024-06-11 23:09:18 -0500

    create server

--- index.js
+++ index.js
@@ -3,8 +3,22 @@ const cors = require('cors')
 const connection = require('./database/connection')
 
 const app = express()
+const puerto = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+
 connection()
 
-app.listen(3000, () => {
-  console.log(`Server listening on`)
+app.get('/', (req, res) => {
+  res.status(200).json({
+    "id": 1,
+    "name": "Leandro Peralta",
+    "username": "Leandro90"
+  }) 
+})
+
+app.listen(puerto, () => {
+  console.log(`Server listening on http://localhost:${puerto}`)
 })
